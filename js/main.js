028d66e45787bc9db9901dcb1cd4ee552f0b023d
Raymond427 <rferrel427@gmail.com>
2018-08-25 13:32:21 -0400

    ARIA

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name;
   li.append(image);
 
   const name = document.createElement('h1');
@@ -178,6 +179,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.role = "button";
+  more.ariaLabel = `View Details for ${restaurant.name}`
   li.append(more)
 
   return li
